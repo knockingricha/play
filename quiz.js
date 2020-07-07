49fe71d988848575da194d31c117e7751125a35e
knockingricha <richabalupuri10@gmail.com>
2020-07-07 19:05:28 +0530

    trial

---
+++ quiz.js
@@ -0,0 +1,22 @@
+var ul=document.getElementById("ul");
+var box=document.getElementById("questionBox");
+var button=document.getElementById("nextButton");
+var opt1=document.getElementById("option1");
+var opt2=document.getElementById("option2");
+var opt3=document.getElementById("option3");
+var opt4=document.getElementById("option4");
+
+var app={
+    question:[
+        {
+        q:"What is my favorite song?",
+    options=[ 'sad', 'romantic', 'pop',' none'],
+    answer: 2
+    }
+
+
+],
+}
+
+
+
